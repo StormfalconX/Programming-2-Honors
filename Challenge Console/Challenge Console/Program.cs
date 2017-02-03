24460764ad03b378c09af55527dac499e6c319c6
Achintya Kumar <achintyasi@gmail.com>
2017-02-03 09:12:54 -0500

    Add files via upload

---
+++ Challenge Console/Challenge Console/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Challenge_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int flag = 0;
+            int counter = 0;
+
+            string tempGrade = string.Empty;
+
+            double totalValue = 0;
+            double averageValue = 0;
+
+            do
+            {
+                
+                Console.WriteLine("Add grades until you stop at -1");
+                tempGrade = Console.ReadLine();
+                try
+                {
+                    if (Convert.ToDouble(tempGrade) == -1)
+                    {
+                        flag = -1;
+                        
+                        averageValue = totalValue / counter;
+                        Console.WriteLine("your average is " + averageValue.ToString("##.##"))
+                            ;
+
+                        Console.ReadLine();
+                    }
+                    else
+                    {
+                        totalValue += Convert.ToDouble(tempGrade);
+                        counter++;
+                    }
+
+                    
+                }
+
+                catch
+                {
+                    Console.WriteLine("enter a number");
+                }
+
+            } while (flag != -1);
+
+           
+        }
+    }
+}
