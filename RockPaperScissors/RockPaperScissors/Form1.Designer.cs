eea6c42dadc8362a0838a655fbfa78d17db9e0a3
Achintya Kumar <achintyasi@gmail.com>
2017-02-20 09:56:59 -0500

    Add files via upload

---
+++ RockPaperScissors/RockPaperScissors/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace RockPaperScissors
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRock = new System.Windows.Forms.Button();
+            this.btnPaper = new System.Windows.Forms.Button();
+            this.btnScissors = new System.Windows.Forms.Button();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.shapeContainer1 = new Microsoft.VisualBasic.PowerPacks.ShapeContainer();
+            this.lineShape1 = new Microsoft.VisualBasic.PowerPacks.LineShape();
+            this.lblSelection = new System.Windows.Forms.Label();
+            this.lblPC = new System.Windows.Forms.Label();
+            this.lblUser = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnRock
+            // 
+            this.btnRock.Location = new System.Drawing.Point(155, 55);
+            this.btnRock.Name = "btnRock";
+            this.btnRock.Size = new System.Drawing.Size(75, 23);
+            this.btnRock.TabIndex = 0;
+            this.btnRock.Text = "Rock";
+            this.btnRock.UseVisualStyleBackColor = true;
+            this.btnRock.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btnPaper
+            // 
+            this.btnPaper.Location = new System.Drawing.Point(155, 105);
+            this.btnPaper.Name = "btnPaper";
+            this.btnPaper.Size = new System.Drawing.Size(75, 23);
+            this.btnPaper.TabIndex = 1;
+            this.btnPaper.Text = "Paper";
+            this.btnPaper.UseVisualStyleBackColor = true;
+            this.btnPaper.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btnScissors
+            // 
+            this.btnScissors.Location = new System.Drawing.Point(155, 151);
+            this.btnScissors.Name = "btnScissors";
+            this.btnScissors.Size = new System.Drawing.Size(75, 23);
+            this.btnScissors.TabIndex = 2;
+            this.btnScissors.Text = "Scissors";
+            this.btnScissors.UseVisualStyleBackColor = true;
+            this.btnScissors.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Location = new System.Drawing.Point(12, 65);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(106, 13);
+            this.lbl1.TabIndex = 3;
+            this.lbl1.Text = "Computer\'s Selection";
+            // 
+            // shapeContainer1
+            // 
+            this.shapeContainer1.Location = new System.Drawing.Point(0, 0);
+            this.shapeContainer1.Margin = new System.Windows.Forms.Padding(0);
+            this.shapeContainer1.Name = "shapeContainer1";
+            this.shapeContainer1.Shapes.AddRange(new Microsoft.VisualBasic.PowerPacks.Shape[] {
+            this.lineShape1});
+            this.shapeContainer1.Size = new System.Drawing.Size(274, 262);
+            this.shapeContainer1.TabIndex = 4;
+            this.shapeContainer1.TabStop = false;
+            // 
+            // lineShape1
+            // 
+            this.lineShape1.Name = "lineShape1";
+            this.lineShape1.X1 = 132;
+            this.lineShape1.X2 = 133;
+            this.lineShape1.Y1 = 37;
+            this.lineShape1.Y2 = 188;
+            // 
+            // lblSelection
+            // 
+            this.lblSelection.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblSelection.Location = new System.Drawing.Point(30, 105);
+            this.lblSelection.Name = "lblSelection";
+            this.lblSelection.Size = new System.Drawing.Size(62, 18);
+            this.lblSelection.TabIndex = 5;
+            // 
+            // lblPC
+            // 
+            this.lblPC.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblPC.Location = new System.Drawing.Point(43, 221);
+            this.lblPC.Name = "lblPC";
+            this.lblPC.Size = new System.Drawing.Size(26, 18);
+            this.lblPC.TabIndex = 6;
+            // 
+            // lblUser
+            // 
+            this.lblUser.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblUser.Location = new System.Drawing.Point(187, 221);
+            this.lblUser.Name = "lblUser";
+            this.lblUser.Size = new System.Drawing.Size(26, 18);
+            this.lblUser.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 197);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Computer Wins";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(167, 197);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Player Wins";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(274, 262);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblUser);
+            this.Controls.Add(this.lblPC);
+            this.Controls.Add(this.lblSelection);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.btnScissors);
+            this.Controls.Add(this.btnPaper);
+            this.Controls.Add(this.btnRock);
+            this.Controls.Add(this.shapeContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRock;
+        private System.Windows.Forms.Button btnPaper;
+        private System.Windows.Forms.Button btnScissors;
+        private System.Windows.Forms.Label lbl1;
+        private Microsoft.VisualBasic.PowerPacks.ShapeContainer shapeContainer1;
+        private Microsoft.VisualBasic.PowerPacks.LineShape lineShape1;
+        private System.Windows.Forms.Label lblSelection;
+        private System.Windows.Forms.Label lblPC;
+        private System.Windows.Forms.Label lblUser;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ RockPaperScissors/RockPaperScissors/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RockPaperScissors
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int playerCounter;
+        int pcCounter;
+
+        int playerSelection;
+        int pcSelection;
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            Button btnClick = new Button();
+            btnClick = (Button)sender;
+            if (btnClick.Text == "Rock")
+            {
+                playerSelection = 1;
+                RockPaperScissors();
+            }
+            else if (btnClick.Text == "Paper")
+            {
+                playerSelection = 2;
+                RockPaperScissors();
+            }
+            else
+            {
+                playerSelection = 3;
+                RockPaperScissors();
+            }
+        }
+
+        public void RockPaperScissors()
+        {
+            Random randInt = new Random();
+
+            pcSelection = randInt.Next(1,4);
+
+            if (playerSelection == 1 & pcSelection == 1)
+            {
+                lblSelection.Text = "Rock";
+                MessageBox.Show("It is a tie");
+            }
+            else if (playerSelection == 1 & pcSelection == 2)
+            {
+                lblSelection.Text = "Paper";
+                MessageBox.Show("Computer Wins");
+                pcCounter++;
+                lblPC.Text = pcCounter.ToString();
+            }
+            else if (playerSelection == 1 & pcSelection == 3)
+            {
+                lblSelection.Text = "Scissors";
+                MessageBox.Show("You Win");
+                playerCounter++;
+                lblUser.Text = playerCounter.ToString();
+            }
+            else if (playerSelection == 2 & pcSelection == 1)
+            {
+
+                lblSelection.Text = "Rock";
+                MessageBox.Show("You Win");
+                playerCounter++;
+                lblUser.Text = playerCounter.ToString();
+            }
+            else if (playerSelection == 2 & pcSelection == 2)
+            {
+                lblSelection.Text = "Paper";
+                MessageBox.Show("It is a tie");
+            }
+            else if (playerSelection == 2 & pcSelection == 3)
+            {
+
+                lblSelection.Text = "Scissors";
+                MessageBox.Show("Computer Win");
+                pcCounter++;
+                lblPC.Text = pcCounter.ToString();
+            }
+            else if (playerSelection == 3 & pcSelection == 1)
+            {
+
+                lblSelection.Text = "Rock";
+                MessageBox.Show("Computer Win");
+                pcCounter++;
+                lblPC.Text = pcCounter.ToString();
+            }
+            else if (playerSelection == 3 & pcSelection == 2)
+            {
+
+                lblSelection.Text = "Paper";
+                MessageBox.Show("You Win");
+                playerCounter++;
+                lblUser.Text = playerCounter.ToString();
+            }
+            else if (playerSelection == 3 & pcSelection == 3)
+            {
+
+                lblSelection.Text = "Rock";
+                MessageBox.Show("It is a tie");
+            }
+        }
+
+        
+        /*private void btnRock_Click(object sender, EventArgs e)
+        {
+            playerSelection = 1;
+            RockPaperScissors();
+
+        }
+
+        private void btnPaper_Click(object sender, EventArgs e)
+        {
+            playerSelection = 2;
+            RockPaperScissors();
+
+        }
+
+        private void btnScissors_Click(object sender, EventArgs e)
+        {
+            playerSelection = 3;
+            RockPaperScissors();
+        }*/
+    }
+}
