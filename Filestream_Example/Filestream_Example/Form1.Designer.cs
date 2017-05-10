c5d949378d993c47c4ce87a0f3645d9b5beadf7b
Achintya Kumar <achintyasi@gmail.com>
2017-05-10 09:51:06 -0400

    Add files via upload

---
+++ Filestream_Example/Filestream_Example/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Filestream_Example
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(85, 80);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(114, 104);
+            this.textBox1.TabIndex = 0;
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(30, 40);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(75, 23);
+            this.btnRead.TabIndex = 1;
+            this.btnRead.Text = "Read File";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(169, 40);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "Save File";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 209);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnRead);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Filestream_Example/Filestream_Example/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Filestream_Example
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            string FileName = Environment.GetFolderPath(Environment.SpecialFolder.Personal) + "\\Programming 2\\DATA\\data01.txt";
+
+            label1.Text = "Editing: " + FileName;
+
+
+
+            FileStream FS = new FileStream(FileName, FileMode.Open, FileAccess.Read, FileShare.None);
+
+            byte[] bText = new byte[FS.Length]; //array of bytes, size of file
+            FS.Read(bText, 0, bText.Length); //read file contents
+            textBox1.Text = System.Text.Encoding.ASCII.GetString(bText); //convert byte array into string for text property
+
+            FS.Close();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            string FileName = Environment.GetFolderPath(Environment.SpecialFolder.Personal) + "\\Programming 2\\DATA\\data01.txt";
+
+            label1.Text = "Saved to: " + FileName;
+
+            FileStream FS = new FileStream(FileName, FileMode.OpenOrCreate, FileAccess.Write, FileShare.None);
+
+            byte[] btext = System.Text.Encoding.ASCII.GetBytes(textBox1.Text); //covert text property contents to byte array
+
+            FS.Write(btext, 0, btext.Length);//write byte array to file from beginning to end
+
+            FS.Close();
+        }
+    }
+}
